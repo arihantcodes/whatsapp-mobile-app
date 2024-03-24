c6ef6569afe87da3ba7a271a316d9af767559bbc
arihant-jain0 <jainari1208@gmail.com>
2024-03-24 17:43:12 +0530

    Delete unused files and update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,43 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { View, TouchableOpacity } from "react-native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Link, Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-import { useColorScheme } from "@/components/useColorScheme";
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
 };
+
+export { ErrorBoundary } from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
@@ -41,44 +54,71 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inTabsGroup = segments[0] === "(auth)";
+
+    if (isSignedIn && !inTabsGroup) {
+      router.replace("/(tabs)/chat");
+    } else if (!isSignedIn) {
+      router.replace("/");
+    }
+  }, [isSignedIn]);
+
+  if (!loaded || !isLoaded) {
+    return <View />;
   }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen
-  name="otp"
-  options={{
-    headerStyle: {
-      backgroundColor: "#000",
-    },
-    headerBackVisible: false,
-    headerTintColor: "#fff",
-    title: " Login with Phone Number",
-    headerTitleStyle: {}
-  }}
-/>
+        name="otp"
+        options={{
+          headerStyle: {
+            backgroundColor: "#000",
+          },
+          headerBackVisible: false,
+          headerTintColor: "#fff",
+          title: " Login with Phone Number",
+          headerTitleStyle: {},
+        }}
+      />
       <Stack.Screen
-  name="verify/[phone]"
-  options={{
-    headerStyle: {
-      backgroundColor: "#000",
-    },
-    headerBackVisible: false,
-    headerTintColor: "#fff",
-    title: " Enter your OTP",
-    headerTitleStyle: {}
-  }}
-/>
-
+        name="verify/[phone]"
+        options={{
+          headerStyle: {
+            backgroundColor: "#000",
+          },
+          headerBackVisible: false,
+          headerTintColor: "#fff",
+          title: " Enter your OTP",
+          headerTitleStyle: {},
+        }}
+      />
+      <Stack.Screen
+        name="(tabs)/chat"
+        options={{
+          headerStyle: {
+            backgroundColor: "#000",
+          },
+          headerBackVisible: false,
+          headerTintColor: "#fff",
+          title: " Talkify",
+          headerTitleStyle: {},
+        }}
+      />
     </Stack>
   );
-}
+};
+
+const RootLayoutNav = () => {
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!}>
+      <InitialLayout />
+    </ClerkProvider>
+  );
+};
+
+export default RootLayoutNav;
--- app/otp.tsx
+++ app/otp.tsx
@@ -1,90 +1,141 @@
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
 import {
   View,
   Text,
+  StyleSheet,
+  Linking,
+  TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
-  Linking,
-  StyleSheet,
-  TouchableOpacity,
-  TouchableOpacityBase,
-} from "react-native";
-import React, { useState } from "react";
-import { useRouter } from "expo-router";
-import Colors from "@/constants/Colors";
-import { Ionicons } from "@expo/vector-icons";
-import MaskInput from "react-native-mask-input";
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
+import MaskInput from 'react-native-mask-input';
+import { isClerkAPIResponseError, useSignIn, useSignUp } from '@clerk/clerk-expo';
 
-const otp = () => {
-  const [loading, setLoading] = useState(false);
-  const [countryCode, setCountryCode] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+const GER_PHONE = [
+  `+`,
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+];
+
+const Page = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
   const router = useRouter();
-  const keyboard = Platform.OS === "android" ? "90" : "0";
-  const keyboardios = Platform.OS === "ios" ? "90" : "0";
-  const openLink = () => {
-    Linking.openURL("https://arihant.us");
-  };
-  const SendOTP = async () => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      router.push(`/verify/${phoneNumber}`);
-    }, 1000);
-  };
-  const TrySign = async () => {};
-  const GER_PHONE = [
-    `+`,
-    /\d/,
-    /\d/,
-    " ",
+  const [loading, setLoading] = useState(false);
+  const { signUp, setActive } = useSignUp();
+  const { signIn } = useSignIn();
 
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    " ",
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-  ];
+  const openLink = () => {
+    Linking.openURL('https://galaxies.dev');
+  };
+
+  const sendOTP = async () => {
+    console.log('sendOTP', phoneNumber);
+    setLoading(true);
+
+    try {
+      await signUp!.create({
+        phoneNumber,
+      });
+      console.log('TESafter createT: ', signUp!.createdSessionId);
+
+      signUp!.preparePhoneNumberVerification();
+
+      console.log('after prepare: ');
+      router.push(`/verify/${phoneNumber}`);
+    } catch (err) {
+      console.log('error', JSON.stringify(err, null, 2));
+
+      if (isClerkAPIResponseError(err)) {
+        if (err.errors[0].code === 'form_identifier_exists') {
+          // User signed up before
+          console.log('User signed up before');
+          await trySignIn();
+        } else {
+          setLoading(false);
+          Alert.alert('Error', err.errors[0].message);
+        }
+      }
+    }
+  };
+
+  const trySignIn = async () => {
+    console.log('trySignIn', phoneNumber);
+
+    const { supportedFirstFactors } = await signIn!.create({
+      identifier: phoneNumber,
+    });
+
+    const firstPhoneFactor: any = supportedFirstFactors.find((factor: any) => {
+      return factor.strategy === 'phone_code';
+    });
+
+    const { phoneNumberId } = firstPhoneFactor;
+
+    await signIn!.prepareFirstFactor({
+      strategy: 'phone_code',
+      phoneNumberId,
+    });
+
+    router.push(`/verify/${phoneNumber}?signin=true`);
+    setLoading(false);
+  };
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }}>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={keyboardVerticalOffset}
+      style={{ flex: 1 }}
+      behavior="padding">
+      {loading && (
+        <View style={[StyleSheet.absoluteFill,]}>
+          <ActivityIndicator size="large" color={Colors.primary} />
+          <Text style={{ fontSize: 18, padding: 10 }}>Sending code...</Text>
+        </View>
+      )}
+
       <View style={styles.container}>
         <Text style={styles.description}>
-          Please Confirm your country code and enter your mobile number{" "}
+          WhatsApp will need to verify your account. Carrier charges may apply.
         </Text>
+
         <View style={styles.list}>
           <View style={styles.listitem}>
             <Text style={styles.countryCode}>India</Text>
-            <Ionicons name="chevron-forward" size={20} color={Colors.gray} />
+            <Ionicons name="chevron-forward" size={20} color={Colors.white} />
           </View>
           <View style={styles.separator} />
 
           <MaskInput
             value={phoneNumber}
-            keyboardType="phone-pad"
-            autoFocus={true}
-            placeholder="(+91)  Your Phone Numbe"
-            placeholderTextColor={Colors.gray}
-            style={styles.input}
+            keyboardType="numeric"
+            autoFocus
+            placeholder="+91 your phone number"
+            placeholderTextColor={Colors.white}
             onChangeText={(masked, unmasked) => {
               setPhoneNumber(masked);
             }}
             mask={GER_PHONE}
+            style={styles.input}
           />
         </View>
-
-        
-
-
-        <TouchableOpacity style={styles.button} onPress={SendOTP}>
-          <Text style={styles.buttonText}>SEND OTP</Text>
-        </TouchableOpacity>
-
 
         <Text style={styles.legal}>
           You must be{' '}
@@ -97,25 +148,36 @@ const otp = () => {
           </Text>
           .
         </Text>
+
+        <View style={{ flex: 1 }} />
+
+        <TouchableOpacity
+          style={[styles.button, phoneNumber !== '' ? styles.enabled : null, { marginBottom: 20 }]}
+          onPress={sendOTP}>
+          <Text style={[styles.buttonText, phoneNumber !== '' ? styles.enabled : null]}>Next</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     padding: 20,
     backgroundColor: "#000",
-    
 
     gap: 20,
   },
+  enabled: {
+    backgroundColor: Colors.white,
+    color: '#000',
+  },
   legal: {
     fontSize: 12,
-    textAlign: 'center',
+    textAlign: "center",
     color: Colors.white,
-
   },
   link: {
     color: Colors.primary,
@@ -180,4 +242,5 @@ const styles = StyleSheet.create({
   },
 });
 
-export default otp;
+
+export default Page;
